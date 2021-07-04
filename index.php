dee6a05e0326194c29fac44b09d3092c287fbeb8
gurupetach <39467151+gurupetach@users.noreply.github.com>
2021-07-04 22:54:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-	session_start();
-	// Redirect the user to login page if he is not logged in.
-	if(!isset($_SESSION['loggedIn'])){
-		header('Location: login.php');
-		exit();
-	}
+	
 	
 	require_once('inc/config/constants.php');
 	require_once('inc/config/db.php');
