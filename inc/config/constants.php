a3b989029254c387fc79749ef5b977c3da8a4614
gurupetach <39467151+gurupetach@users.noreply.github.com>
2021-07-04 18:16:19 +0300

    Add files via upload

---
+++ inc/config/constants.php
@@ -0,0 +1,21 @@
+<?php
+	// Root url for the site
+	define('ROOT_URL', 'http://localhost/assignment/d/index.php');
+	
+	
+	// Database parameters
+	// Data source name
+	define('DSN', 'mysql:host=localhost;dbname=kefis');
+	
+	// Hostname
+	define('DB_HOST', 'localhost');
+	
+	// DB user
+	define('DB_USER', 'root');
+	
+	// DB password
+	define('DB_PASSWORD', '');
+	
+	// DB name
+	define('DB_NAME', 'efis');
+?>
